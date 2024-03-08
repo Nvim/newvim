ec269b5738612d478d1653dcd999bbc65ee920cc
naim <naimssj4@gmail.com>
2024-03-08 22:14:05 +0100

    codeium + dap config

--- lua/oui/plugins/codeium.lua
+++ lua/oui/plugins/codeium.lua
@@ -1,14 +1,33 @@
 local codeium = {
 	"Exafunction/codeium.vim",
-	event = "VeryLazy",
+	event = "BufEnter",
 	config = function()
-		-- Change '<C-g>' here to any keycode you like.
-		-- vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-		-- vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-		-- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-		-- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+		local s = vim.keymap.set
+		local opts = { expr = true, silent = true }
+
+		vim.g.codeium_disable_bindings = 1 --disables defaults
+
+		s("i", "<S-Tab>", function()
+			return vim.fn["codeium#Accept"]()
+		end, opts)
+		s("i", "<c-;>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, opts)
+		s("i", "<c-,>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, opts)
+		s("i", "<c-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, opts)
+
+		s("n", "<leader><c-x>", function()
+			vim.g.codeium_enabled = false
+		end, opts)
+		s("n", "<leader><c-e>", function()
+			vim.g.codeium_enabled = true
+		end, opts)
 	end,
 }
 
 -- return codeium
-return {}
+return codeium
