a698b2233ffaf1701c0fb7d3dbfd0a2e239999c1
naim <naimssj4@gmail.com>
2024-04-21 23:39:22 +0200

    add nix lsp

--- lua/oui/utils/server_list.lua
+++ lua/oui/utils/server_list.lua
@@ -1,38 +1,39 @@
 -- LSP Server List:
 local ensure_installed = {
-  lsps = {
-    "bashls",
-    "clangd",
-    "jdtls",
-    "intelephense",
-    "html",
-    "cssls",
-    "volar",
-    "emmet_language_server",
-    "tsserver",
-    -- "eslint",
-    "lua_ls",
-    "pyright",
-    "glsl_analyzer",
-    "texlab",
-  },
+	lsps = {
+		"bashls",
+		"clangd",
+		"jdtls",
+		"intelephense",
+		"html",
+		"cssls",
+		"volar",
+		"emmet_language_server",
+		"tsserver",
+		-- "eslint",
+		"lua_ls",
+		"pyright",
+		"glsl_analyzer",
+		"nil_ls",
+		"texlab",
+	},
 
-  others = {
-    -- lint
-    "eslint_d", -- linter
-    "mypy",     --linter(static type check)
-    "ruff",     --fast linting
+	others = {
+		-- lint
+		"eslint_d", -- linter
+		"mypy", --linter(static type check)
+		"ruff", --fast linting
 
-    -- format
-    "prettier",        -- formatter
-    "blade-formatter", --formatter
-    "pint",            --formatter
-    "stylua",          -- formatter
-    "isort",           --formatter
-    "black",           --formatter
-    "latexindent",     --formatter
-  }
+		-- format
+		"prettier", -- formatter
+		"blade-formatter", --formatter
+		"pint", --formatter
+		"stylua", -- formatter
+		"isort", --formatter
+		"black", --formatter
+		"latexindent", --formatter
+		"nixpkgs-fmt",
+	},
 }
-
 
 return ensure_installed
