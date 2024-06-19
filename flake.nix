c2dcfc12d25e7ec141c64e1e6df2819d83d509e8
naim <naimssj4@gmail.com>
2024-06-19 22:25:27 +0200

    overlay

--- flake.nix
+++ flake.nix
@@ -2,57 +2,69 @@
   description = "My Neovim config Flake";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.neovim-nightly-overlay.url =
-    "github:nix-community/neovim-nightly-overlay";
+  inputs.neovim.url = "github:nix-community/neovim-nightly-overlay";
 
-  outputs = inputs@{ self, nixpkgs, neovim-nightly-overlay, ... }: {
-    packages = let
+  outputs = inputs@{ self, nixpkgs, neovim, ... }:
+    let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      extraPackages = with pkgs; [
-        # Utils:
-        fzf
-        xclip
 
-        # Language Support:
-        yarn
-        php
-        php83Packages.composer
-        python311
-        python311Packages.pip
-
-        # LSP:
-        clang-tools # for clangd
-        lua-language-server
-        nixd
-        vscode-langservers-extracted
-        emmet-language-server
-        ruff-lsp
-        nodePackages.volar
-        nodePackages.typescript-language-server
-
-        # Format:
-        nixfmt
-        prettierd
-        stylua
-        isort
-        black
-        shfmt
-      ];
-    in {
-      x86_64-linux.default = pkgs.neovim.override {
-        configure = {
-          viAlias = true;
-          customRC = ''
-            lua << EOF
-            require("oui").init()
-            require("oui.lazy")
-            EOF
-          '';
-        };
-        extraMakeWrapperArgs =
-          ''--suffix PATH : "${pkgs.lib.strings.makeBinPath extraPackages}"'';
+      overlayFlakeInputs = prev: final: {
+        # neovim = neovim.packages.x86_64-linux.neovim;
+        neovim = nixpkgs.legacyPackages.x86_64-linux.neovim;
       };
-    };
-  };
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ overlayFlakeInputs overlayMyNeovim ];
+      };
+
+      overlayMyNeovim = prev: final:
+        let
+          extraPackages = with pkgs; [
+            # Utils:
+            fzf
+            xclip
+
+            # Language Support:
+            yarn
+            php
+            php83Packages.composer
+            python311
+            python311Packages.pip
+
+            # LSP:
+            clang-tools # for clangd
+            lua-language-server
+            nixd
+            vscode-langservers-extracted
+            emmet-language-server
+            ruff-lsp
+            nodePackages.volar
+            nodePackages.typescript-language-server
+
+            # Format:
+            nixfmt
+            prettierd
+            stylua
+            isort
+            black
+            shfmt
+          ];
+        in {
+          myNeovim = pkgs.neovim.override {
+            configure = {
+              viAlias = true;
+              customRC = ''
+                lua << EOF
+                  require("oui").init()
+                  require("oui.lazy")
+                EOF
+              '';
+            };
+            extraMakeWrapperArgs = ''
+              --suffix PATH : "${pkgs.lib.strings.makeBinPath extraPackages}"'';
+          };
+        };
+
+    in { packages.x86_64-linux.default = pkgs.myNeovim; };
 }
