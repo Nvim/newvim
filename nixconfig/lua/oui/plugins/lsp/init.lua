b2f3e46a5fba239b5750b08c979b73e01233ea57
= <=>
2024-11-13 09:18:50 +0100

    add mason for non-nix setup

--- nixconfig/lua/oui/plugins/lsp/init.lua
+++ nixconfig/lua/oui/plugins/lsp/init.lua
@@ -111,6 +111,22 @@ local M = {
 		lazy = true,
 	},
 	{
+		"williamboman/mason.nvim",
+		cmd = { "Mason", "MasonInstall", "MasonUpdate", "MasonInstallAll" },
+		opts = {
+			-- ensure_installed = require("utils.server_list").others,
+			ensure_installed = {},
+		},
+		config = function(_, opts)
+			require("mason").setup(opts)
+			vim.api.nvim_create_user_command("MasonInstallAll", function()
+				vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+			end, {})
+
+			vim.g.mason_binaries_list = opts.ensure_installed
+		end,
+	},
+	{
 		"neovim/nvim-lspconfig",
 		cmd = { "LspInfo", "LspInstall", "LspStart" },
 		event = { "BufReadPre", "BufNewFile" },
