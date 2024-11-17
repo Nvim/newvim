1d6f2a6a409a9ffb3f0e8d082344d81d09c175bc
Naim <naimssj4@gmail.com>
2024-11-17 21:51:41 +0100

    Fix: gdb dap adapter

--- nixconfig/lua/oui/plugins/dap.lua
+++ nixconfig/lua/oui/plugins/dap.lua
@@ -1,8 +1,8 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    {
-      "rcarriga/nvim-dap-ui",
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		{
+			"rcarriga/nvim-dap-ui",
       -- stylua: ignore
       keys = {
         {
@@ -11,116 +11,117 @@ return {
           { desc = "DAPUI Eval" },
         },
       },
-      opts = {},
-      config = function(_, opts)
-        local dap, dapui = require("dap"), require("dapui")
-        dapui.setup(opts)
-        dap.listeners.before.attach.dapui_config = function()
-          dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-          dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-          dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-          dapui.close()
-        end
-      end,
-    },
-    "nvim-neotest/nvim-nio",
-    { "theHamsta/nvim-dap-virtual-text", opts = {} },
-    {
-      "leoluz/nvim-dap-go",
-      opts = {},
-    },
-  },
+			opts = {},
+			config = function(_, opts)
+				local dap, dapui = require("dap"), require("dapui")
+				dapui.setup(opts)
+				dap.listeners.before.attach.dapui_config = function()
+					dapui.open()
+				end
+				dap.listeners.before.launch.dapui_config = function()
+					dapui.open()
+				end
+				dap.listeners.before.event_terminated.dapui_config = function()
+					dapui.close()
+				end
+				dap.listeners.before.event_exited.dapui_config = function()
+					dapui.close()
+				end
+			end,
+		},
+		"nvim-neotest/nvim-nio",
+		{ "theHamsta/nvim-dap-virtual-text", opts = {} },
+		{
+			"leoluz/nvim-dap-go",
+			opts = {},
+		},
+	},
 
-  -- OPTS
-  opts = function()
-    local dap, dapui = require("dap"), require("dapui")
-    require("nvim-dap-virtual-text").setup()
-    require("dap-go").setup()
-    require("overseer").enable_dap()
-    if not dap.adapters["codelldb"] then
-      require("dap").adapters["codelldb"] = {
-        type = "server",
-        host = "localhost",
-        port = "${port}",
-        executable = {
-          command = "codelldb",
-          args = {
-            "--port",
-            "${port}",
-          },
-        },
-      }
-    end
-    if not dap.adapters["lldb"] then
-      require("dap").adapters["lldb"] = {
-        type = "executable",
-        command = "lldb-dap", -- adjust as needed, must be absolute path
-        name = "lldb",
-      }
-    end
-    if not dap.adapters["lldb"] then
-      dap.adapters.gdb = {
-        type = "executable",
-        command = "gdb",
-        args = { "-i", "dap" },
-      }
-    end
-    for _, lang in ipairs({ "c", "cpp" }) do
-      dap.configurations[lang] = {
-        {
-          name = "Launch file (gdb)",
-          type = "gdb",
-          request = "launch",
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-          end,
-          cwd = "${workspaceFolder}",
-          stopAtBeginningOfMainSubprogram = false,
-        },
-        {
-          name = "Launch file (lldb)",
-          type = "lldb",
-          request = "launch",
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-          end,
-          cwd = "${workspaceFolder}",
-          stopOnEntry = false,
-          args = {},
-        },
-        {
-          name = "Attach to process (lldb)",
-          type = "lldb",
-          request = "attach",
-          pid = require("dap.utils").pick_process,
-          args = {},
-          cwd = "${workspaceFolder}",
-        },
-        {
-          type = "codelldb",
-          request = "launch",
-          name = "Launch file (Codelldb)",
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-          end,
-          cwd = "${workspaceFolder}",
-        },
-        {
-          type = "codelldb",
-          request = "attach",
-          name = "Attach to process (Codelldb)",
-          pid = require("dap.utils").pick_process,
-          cwd = "${workspaceFolder}",
-        },
-      }
-    end
-  end,
+	-- OPTS
+	opts = function()
+		local dap, dapui = require("dap"), require("dapui")
+		require("nvim-dap-virtual-text").setup()
+		require("dap-go").setup()
+		require("overseer").enable_dap()
+
+		if not dap.adapters["gdb"] then
+			require("dap").adapters["gdb"] = {
+				type = "executable",
+				command = "gdb",
+				args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+			}
+		end
+		if not dap.adapters["codelldb"] then
+			require("dap").adapters["codelldb"] = {
+				type = "server",
+				host = "localhost",
+				port = "${port}",
+				executable = {
+					command = "codelldb",
+					args = {
+						"--port",
+						"${port}",
+					},
+				},
+			}
+		end
+		if not dap.adapters["lldb"] then
+			require("dap").adapters["lldb"] = {
+				type = "executable",
+				command = "lldb-dap", -- adjust as needed, must be absolute path
+				name = "lldb",
+			}
+		end
+		for _, lang in ipairs({ "c", "cpp" }) do
+			dap.configurations[lang] = {
+				{
+					name = "Launch file (gdb)",
+					type = "gdb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopAtBeginningOfMainSubprogram = false,
+				},
+				{
+					name = "Launch file (lldb)",
+					type = "lldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+				},
+				{
+					name = "Attach to process (lldb)",
+					type = "lldb",
+					request = "attach",
+					pid = require("dap.utils").pick_process,
+					args = {},
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "codelldb",
+					request = "launch",
+					name = "Launch file (Codelldb)",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "codelldb",
+					request = "attach",
+					name = "Attach to process (Codelldb)",
+					pid = require("dap.utils").pick_process,
+					cwd = "${workspaceFolder}",
+				},
+			}
+		end
+	end,
 
   -- Keys
   -- stylua: ignore
@@ -138,18 +139,18 @@ return {
     { "<F5>",       function() require("dap").step_back() end,         { desc = "DAP Step Back" } },
   },
 
-  -- Config:
-  config = function()
-    -- setup dap config by VsCode launch.json file
-    -- local vscode = require("dap.ext.vscode")
-    -- local json = require("plenary.json")
-    -- vscode.json_decode = function(str)
-    --   return vim.json.decode(json.json_strip_comments(str))
-    -- end
-    local sign = vim.fn.sign_define
+	-- Config:
+	config = function()
+		-- setup dap config by VsCode launch.json file
+		-- local vscode = require("dap.ext.vscode")
+		-- local json = require("plenary.json")
+		-- vscode.json_decode = function(str)
+		--   return vim.json.decode(json.json_strip_comments(str))
+		-- end
+		local sign = vim.fn.sign_define
 
-    sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
-    sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
-    sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
-  end,
+		sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+		sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+		sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+	end,
 }
