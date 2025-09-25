8df98cf91d87d684c5407dd8e8ea28614061cbbf
Naim <naimssj4@gmail.com>
2025-09-25 09:18:59 +0200

    glsl

--- lua/oui/core/autocommands.lua
+++ lua/oui/core/autocommands.lua
@@ -1,4 +1,16 @@
-vim.api.nvim_create_autocmd(
-  { "BufNewFile", "BufRead" },
-  { pattern = { "*.vs", "*.fs", "*.vert", "*.frag", "*.shader" }, command = [[set ft=glsl]] }
-)
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = { "*.vs", "*.fs", "*.vert", "*.frag", "*.shader", "*.glsl" },
+	callback = function()
+		vim.bo.filetype = "glsl"
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "glsl",
+  callback = function()
+    local opt = vim.opt_local
+    opt.tabstop = 4
+    opt.softtabstop = 4
+    opt.shiftwidth = 4
+  end,
+})
